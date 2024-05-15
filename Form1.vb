0c1e162b43a403c0f157bef24f59a82fd8336a29
Christine T Munhamo <164712654+402310215chris@users.noreply.github.com>
2024-05-15 15:54:57 +0200

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1 As Integer
+        Dim num2 As Integer
+
+
+        num1 = Val(TextBox1.Text)
+        num2 = Val(TextBox2.Text)
+
+        MsgBox(num1 + num2)
+    End Sub
+End Class
