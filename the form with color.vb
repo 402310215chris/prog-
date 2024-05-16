78ff2ef385d499275f9d88017d9cf37bfd61af68
Christine T Munhamo <164712654+402310215chris@users.noreply.github.com>
2024-05-16 11:46:54 +0200

    Add files via upload

---
+++ the form with color.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = "Christine"
+        TextBox1.BackColor = Color.Aqua
+        Button1.BackColor = Color.Teal
+        Me.BackColor = Color.AliceBlue
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Form2.Show()
+
+    End Sub
+End Class
