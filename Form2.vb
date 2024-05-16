5264eb995f4e6cec59e9e71a53c9ece3aa190b27
Christine T Munhamo <164712654+402310215chris@users.noreply.github.com>
2024-05-16 11:43:33 +0200

    Add files via upload

---
+++ Form2.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form2
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim base, height, area As Double
+
+
+        base = Val(TextBox1.Text)
+        height = Val(TextBox2.Text)
+
+
+        area = 0.5 * base * height
+        TextBox3.Text = area
+
+
+
+
+    End Sub
+End Class
